31fedf882df696f50b27a27d4587707657199642
VictorCid <victorcidpereira@gmail.com>
2023-04-19 07:31:10 -0300

    feat: initial commit

---
+++ back-end/tests/integration/app.spec.ts
@@ -0,0 +1,33 @@
+import prisma from "database";
+import httpStatus from "http-status";
+import app from "server";
+import supertest from "supertest";
+
+const agent = supertest(app);
+
+describe("integration test", () => {
+  beforeEach(async () => {
+    await prisma.user.deleteMany();
+  });
+
+  it("should register a user", async () => {
+    const { status } = await agent.get("/add");
+    expect(status).toBe(httpStatus.CREATED);
+
+    const users = await prisma.user.findMany();
+    expect(users).toHaveLength(1);
+  });
+
+  it("should return all users", async () => {
+    await prisma.user.createMany({
+      data: [
+        { name: "teste1", email: "teste1@gmail.com" },
+        { name: "teste2", email: "teste2@gmail.com" },
+      ],
+    });
+
+    const response = await agent.get("/all");
+    expect(response.status).toBe(httpStatus.OK);
+    expect(response.body).toHaveLength(2);
+  });
+});
---
+++ front-end/src/components/App.jsx
@@ -0,0 +1,18 @@
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+import Home from "./Home";
+import Names from "./Names";
+import NotFoundComponent from './NotFoundComponent';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/"  element={<Home />} />
+        <Route path="/names" element={<Names />} />
+        <Route path="*" element={<NotFoundComponent />} />
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App;
